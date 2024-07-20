b5057d9febcc75913694d097d9c6632476ab87a9
Ciutre Ramon <ramonciutre7@gmail.com>
2024-07-20 15:27:43 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import css from './App.module.css';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
@@ -6,60 +6,67 @@ import Button from './Button';
 import Modal from './Modal';
 import { Circles } from 'react-loader-spinner';
 
-export class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      images: [],
-      page: 1,
-      showModal: false,
-      modalImageSrc: '',
-      modalImageAlt: '',
-      query: 'cat',
-      loading: false,
-    };
-  }
-  handleValue = search => {
-    this.setState({ query: search, page: 1, images: [] }, this.fetchImages);
+export const App = (
+  images,
+  showModal,
+  modalImageSrc,
+  modalImageAlt,
+  loading
+) => {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [modalImageSrc, setModalImageSrc] = useState('');
+  const [modalImageAlt, setModalImageAlt] = useState('');
+  const [query, setQuery] = useState('cat');
+  const [loading, setLoading] = useState(false);
+
+  const API_KEY = '44948375-490a2b5531ef23b5637f1620a';
+  const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+  const handleValue = search => {
+    setQuery(search);
+    setPage(1);
+    setImages([]);
   };
 
-  componentDidMount() {
-    this.fetchImages();
-  }
+  useEffect(() => {
+    if (query) {
+      fetchImages();
+    }
+  }, [query, page, fetchImages]);
 
-  fetchImages = async () => {
-    this.setState({ loading: true });
-    const { query, page } = this.state;
-    const API_KEY = '44948375-490a2b5531ef23b5637f1620a';
-    const URL = `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+  const fetchImages = useCallback(async () => {
+    setLoading(true);
+
+    const URL = `${BASE_URL}&q=${query}&page=${page}`;
 
     try {
       const response = await fetch(URL);
       const data = await response.json();
-      this.setState(prevState => ({
-        images: [
-          ...prevState.images,
-          ...data.hits
-            .map(hit => ({
-              id: hit.id,
-              src: hit.webformatURL,
-              alt: hit.tags,
-              largeSrc: hit.largeImageURL,
-            }))
-            .filter(
-              newImage =>
-                !prevState.images.some(image => image.id === newImage.id)
-            ),
-        ],
-        loading: false,
-      }));
+
+      setImages(prevImages => [
+        ...prevImages,
+        ...data.hits
+          .map(hit => ({
+            id: hit.id,
+            src: hit.webformatURL,
+            alt: hit.tags,
+            largeSrc: hit.largeImageURL,
+          }))
+          .filter(
+            newImage => !prevImages.some(image => image.id === newImage.id)
+          ),
+      ]);
+
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching images:', error);
-      this.setState({ loading: false });
+      setLoading(false);
     }
-  };
+  }, [query, page]);
 
-  loadMoreImages = () => {
+  const loadMoreImages = () => {
     this.setState(
       prevState => ({ page: prevState.page + 1 }),
       this.fetchImages
@@ -82,35 +89,27 @@ export class App extends React.Component {
     });
   };
 
-  render() {
-    const { images, showModal, modalImageSrc, modalImageAlt, loading } =
-      this.state;
-    return (
-      <div className={css.App}>
-        <Searchbar onSubmit={this.handleValue} />
-        {loading && (
-          <div className={css.Loader}>
-            <Circles
-              height="80"
-              width="80"
-              color="#7703fc"
-              ariaLabel="circles-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-            />
-          </div>
-        )}
-        <ImageGallery items={images} onImageClick={this.openModal} />
-        {images.length > 0 && <Button onClick={this.loadMoreImages} />}
-        {showModal && (
-          <Modal
-            src={modalImageSrc}
-            alt={modalImageAlt}
-            onClose={this.closeModal}
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={handleValue} />
+      {loading && (
+        <div className={css.Loader}>
+          <Circles
+            height="80"
+            width="80"
+            color="#7703fc"
+            ariaLabel="circles-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
           />
-        )}
-      </div>
-    );
-  }
-}
+        </div>
+      )}
+      <ImageGallery items={images} onImageClick={openModal} />
+      {images.length > 0 && <Button onClick={loadMoreImages} />}
+      {showModal && (
+        <Modal src={modalImageSrc} alt={modalImageAlt} onClose={closeModal} />
+      )}
+    </div>
+  );
+};
